b35a042521aa151ea1b3a6138fc13908b87b7886
ibiayorinde <iaayorinde@gmail.com>
2025-06-27 00:10:47 -0400

    Add basic dashboard page

---
+++ pages/components/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+      <p>This is where you’ll see your course progress.</p>
+    </Layout>
+  )
+}
